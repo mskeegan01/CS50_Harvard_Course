bceb017a43d95e70106679ec98e009d6b4a9c2b6
Martin Straußberger <martin.straussberger@code.berlin>
2018-02-08 18:33:44 +0100

    Create credit.c

---
+++ credit.c
@@ -0,0 +1,22 @@
+Takes me few more trys, before I am able to post credit =D
+
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+
+//adding variables
+void print_card_brand(long long ccn);
+bool checksum(long long ccn, int len);
+bool checksum(long long ccn, int len);
+int length(long long n);
+void check_validity(long long ccn);
+
+int main(void)
+{
+
+    //Input
+    long long credit_card;
+    do 
+    
+    ... to be continued
